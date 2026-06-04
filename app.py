303114c96f8187d6d4bdeec9c7518f068ad4d845
Boby Gupta <bobygupta603@gmail.com>
2026-06-04 20:11:20 +0530

    Refactor context retrieval based on PDF length

--- app.py
+++ app.py
@@ -116,8 +116,13 @@ if st.sidebar.button('Skill Gap Analyzer'):
     if data_path and query is not None:
         pdf_text=pdf_extract(data_path)
         index,pdf=RAG(pdf_text)
-        chunks=Retrieve(query,index,pdf)    
-        answer=extract_skills(pdf,query)
+        if len(pdf)>6000:
+            chunks = Retrieve(query,index,pdf)
+            context = "\n".join(chunks[:3])
+        else:
+            chunks=Retrieve(query,index,pdf)
+            context = "\n".join(chunks)
+        answer=extract_skills(context,query)
         st.success(answer)
     else:
         st.error('Please fill above fields')
@@ -126,8 +131,13 @@ if st.sidebar.button('Resume Analyzer'):
     if data_path and query is not None:
         pdf_text=pdf_extract(data_path)
         index,pdf=RAG(pdf_text)
-        chunks=Retrieve(query,index,pdf)
-        score,ans=ATS(pdf,query)
+        if len(pdf)>6000:
+            chunks = Retrieve(query,index,pdf)
+            context = "\n".join(chunks[:3])
+        else:
+            chunks=Retrieve(query,index,pdf)
+            context = "\n".join(chunks)
+        score,ans=ATS(context,query)
         st.success(f"ATS score is  {score}")
         st.success(ans)
     else:
@@ -153,7 +163,13 @@ if st.session_state.page=="Document_chat":
             st.session_state.messages.append({"role": "user","content": data})
             pdf_text=pdf_extract(data_path)
             index,pdf=RAG(pdf_text)
-            answer=chat(data,pdf)
+            if len(pdf)>6000:
+                chunks = Retrieve(query,index,pdf)
+                context = "\n".join(chunks[:3])
+            else:
+                chunks=Retrieve(query,index,pdf)
+                context = "\n".join(chunks)
+            answer=chat(data,context)
             st.session_state.messages.append({"role": "assistant","content": answer})    
             st.rerun()     
     else:
